7f7f12440013beecae712f98e014b07eac5f205b
Antoine Vigneau <antoine.vigneau@sonarsource.com>
2025-10-07 15:20:37 +0200

    commit

--- src/file1.js
+++ src/file1.js
@@ -1,58 +1,108 @@
-// RSPEC-7060
-import file1 from './file1'; // Noncompliant
-const file1 = require('./file1'); // Noncompliant
+// "Array-mutating methods should not be used misleadingly"
+const arr1 = [1, 2, 3];
+const arr2 = arr1.reverse();
 
-// RSPEC-6859
-import { file1 } from '/Users/antoinevigneau/Documents/Experiments/hackaton-2025/src/file1.js';
+// "Array.prototype.sort() and Array.prototype.toSorted() should use a compare function"
+const arr3 = [1, 10, 2];
+arr3.sort();
 
-// RSPEC-6749
-function Fragment() {
-  return (
-    <><Foo /></>;    // Noncompliant: The fragment has only one child
-    <p><>foo</></p>; // Noncompliant: The fragment is the child of the HTML element 'p'
-  );
+// "Non-existent operators =+, =- and =!"
+let x = 1;
+x =+ 2;
+
+// "NaN should not be used in comparisons"
+if (x === NaN) {
+  console.log("x is NaN");
 }
 
-// RSPEC-6747
-class Welcome extends React.Component {
-  render() {
-    return <div class="hello">Hello, World!</div>; // Noncompliant: 'class' is a reserved keyword in JavaScript
+// "Wrapper objects should not be used for primitive types"
+let y = new Number("0");
+
+// "Array constructors should not be used"
+const arr4 = new Array(1, 2, 3);
+
+// "Statements should end with semicolons"
+var z = 1;;
+function foo() { };
+
+// "switch statements should have default clauses"
+const myVar = 1;
+switch (myVar) {
+  case 1:
+    console.log("1");
+    break;
+}
+
+// "Jump statements should not be redundant"
+function redundantJump(x) {
+  if (x == 1) {
+    console.log("x == 1");
+    return;
   }
 }
 
-// RSPEC-6679
-if (value !== value){ // Noncompliant: use Number.isNaN()
-    processNaN(value);
+// "The base should be provided to parseInt"
+parseInt("010");
+
+// "Unchanged variables should be marked as const"
+let a = 1;
+console.log(a);
+
+// "Variables should be declared with let or const"
+var b = 2;
+
+// "Regular expressions should not contain multiple spaces"
+const regex = /Hello,   world!/;
+
+// "If statements should not be the only statement in else blocks"
+if (a > 1) {
+    console.log("a is greater than 1");
+} else {
+    if (a < 1) {
+        console.log("a is less than 1");
+    }
 }
 
-// RSPEC-6676
-let obj = {
-    checkThis() {
-        this === obj; // true, if called the normal way: obj.checkThis()
+// "Unnecessary constructors should be removed"
+class MyClass {
+    constructor() {
     }
+}
+
+// "Variables should not be initialized to undefined"
+let myUndefined = undefined;
+
+// "Ternary operator should not be used instead of simpler alternatives"
+const myBoolean = a > 1 ? true : false;
+
+// "Extra boolean casts should be removed"
+const anotherBoolean = !!(a > 1);
+
+// "Literals should not be thrown"
+throw "error";
+
+// "Template strings should be used instead of concatenation"
+const myString = "a is " + a;
+
+// "The global this object should not be used"
+console.log(this);
+
+// "All code should be reachable"
+function unreachable() {
+    return;
+    console.log("unreachable");
+}
+
+// "Member names should not be duplicated within a class or object literal"
+const myObject = {
+    prop: 1,
+    prop: 2
 };
 
-let otherObject = {};
-
-obj.checkThis.call(otherObject); // this === otherObject, if called this way
-
-// RSPEC-6666
-foo.apply(undefined, args); // Noncompliant: use spread syntax instead of .apply()
-foo.apply(null, args); // Noncompliant: use spread syntax instead of .apply()
-obj.foo.apply(obj, args); // Noncompliant: use spread syntax instead of .apply()
-
-// RSPEC-6661
-const a = Object.assign({}, foo); // Noncompliant: Use spread syntax to clone or merge objects
-const b = Object.assign({}, foo, bar); // Noncompliant: Use spread syntax to clone or merge objects
-const c = Object.assign({foo: 123}, bar); // Noncompliant: Use spread syntax to clone or merge objects
-const d = Object.assign({}); // Noncompliant: Use spread syntax to clone or merge objects
-
-// RSPEC-6660 
-if (condition1) {
-    // ...
-} else {
-    if (condition2) {  // Noncompliant: 'if' statement is the only statement in the 'else' block
-        // ...
-    }
+// "Local variables should not be declared and then immediately returned or thrown"
+function immediateReturn() {
+    const a = 1;
+    return a;
 }
+
 
